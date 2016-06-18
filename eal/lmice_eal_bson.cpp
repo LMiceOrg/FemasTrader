533cf3c073451201cd3b05e84afda91f04b0aa27
swuser <swuser@test.com>
2016-06-18 23:02:38 +0800

    huangxh：add sourcefile and lib file for bson feature

---
+++ eal/lmice_eal_bson.cpp
@@ -0,0 +1,162 @@
+#include "lmice_eal_bson.h"
+
+const char * EalBson::GetJsonData()
+{
+	char * strJson = NULL;
+	strJson = bson_as_json(m_ptr_bson, NULL);
+	return strJson;
+}
+
+int EalBson::AppendArray( const char*key, EalBson *bsonObj )
+{
+	if( key && bsonObj )
+	{
+		if( bson_append_array( m_ptr_bson, key, -1, bsonObj->GetObj() ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+int EalBson::AppendDocument( const char *key, EalBson *bsonObj )
+{
+	if( key && bsonObj )
+	{
+		if( bson_append_document( m_ptr_bson, key, -1, bsonObj->GetObj() ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+/*
+int EalBson::AppendBson( const char *key, EalBson *bsonObj )
+{
+	bson_iter_t iter;
+	
+}*/
+
+int EalBson::AppendBool( const char *key, bool value )
+{
+	if( key )
+	{
+		if( bson_append_bool( m_ptr_bson, key, -1, value ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+
+int EalBson::AppendDouble( const char *key, double value )
+{
+	if( key )
+	{
+		if( bson_append_double( m_ptr_bson, key, -1, value ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+
+}
+
+int EalBson::AppendInt32( const char *key, int32_t value )
+{
+	if( key )
+	{
+		if( bson_append_int32( m_ptr_bson, key, -1, value ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+int EalBson::AppendInt64( const char *key, int64_t value )
+{
+	if( key )
+	{
+		if( bson_append_int64( m_ptr_bson, key, -1, value ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+int EalBson::AppendNull( const char *key )
+{
+	if( key )
+	{
+		if( bson_append_null( m_ptr_bson, key, -1 ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+int EalBson::AppendUtf8( const char *key, const char *value )
+{
+	if( key && value )
+	{
+		if( bson_append_utf8( m_ptr_bson, key, -1, value, -1 ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+	
+int EalBson::AppendTimeT( const char *key, time_t value )
+{
+	if( key )
+	{
+		if( bson_append_time_t( m_ptr_bson, key, -1, value ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+int EalBson::AppendTimeStamp( const char *key, uint32_t timestamp, uint32_t increment )
+{
+	uint64_t value = ((((uint64_t)timestamp) << 32) | ((uint64_t)increment));
+   	value = BSON_UINT64_TO_LE (value);
+		
+	if( key )
+	{
+		if( bson_append_time_t( m_ptr_bson, key, -1, value ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+int EalBson::AppendNowUtc( const char *key )
+{
+	if( key )
+	{
+		if( bson_append_now_utc( m_ptr_bson, key, -1 ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+int EalBson::AppendDateTime( const char *key, int64_t value )
+{
+	if( key )
+	{
+		if( bson_append_date_time( m_ptr_bson, key, -1, value ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+int EalBson::AppendTimeVal( const char *key, struct timeval *value )
+{
+	if( key && value )
+	{
+		if( bson_append_timeval( m_ptr_bson, key, -1, value ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+int EalBson::AppendUndefined( const char *key )
+{
+	if( key )
+	{
+		if( bson_append_undefined( m_ptr_bson, key, -1 ) )
+			return m_ptr_bson->len;
+	}
+	return -1;
+}
+
+
+
---
+++ eal/lmice_eal_bson.h
@@ -0,0 +1,80 @@
+#ifndef LMIC_EAL_BSON_H
+#define LMIC_EAL_BSON_H
+
+//#include "include/bson.h"
+#include <bson.h>
+
+using namespace std;
+
+class EalBson
+{
+
+public:
+	EalBson()
+	{
+		m_ptr_bson = new bson_t;
+		bson_init(m_ptr_bson);
+	}
+
+	~EalBson()
+	{
+		if( m_ptr_bson )
+		{
+			bson_reinit(m_ptr_bson);
+			bson_destroy(m_ptr_bson);
+		}
+	}
+
+//get bson object info
+public:
+	//get json string of this bson obj
+	const char *GetJsonData();
+
+	//get bson data buf
+	const uint8_t *GetBsonData()
+	{
+		if( m_ptr_bson->len > 0 )
+		{
+			return bson_get_data(m_ptr_bson);
+		}
+		return NULL;
+	}
+
+	//get bson data len
+	int GetLen()
+	{
+		return m_ptr_bson->len;
+	}
+
+	bson_t *GetObj()
+	{
+		return m_ptr_bson;
+	}
+
+//function: insert bson value
+//para    : 1 key, [2 value]
+//return  : <0 failed, >0 current bson buf length 
+
+public:	
+	int AppendArray( const char*key, EalBson *bsonObj );
+	int AppendDocument( const char *key, EalBson *bsonObj );
+	int AppendBool( const char *key, bool value );
+	int AppendDouble( const char *key, double value );
+	int AppendInt32( const char *key, int32_t value );
+	int AppendInt64( const char *key, int64_t value );
+	int AppendNull( const char *key );
+	int AppendUtf8( const char *key, const char *value );
+	int AppendTimeT( const char *key, time_t value );
+	int AppendTimeStamp( const char *key, uint32_t timestamp, uint32_t increment );
+	int AppendNowUtc( const char *key );
+	int AppendDateTime( const char *key, int64_t value );
+	int AppendTimeVal( const char *key, struct timeval *value );
+	int AppendUndefined( const char *key );
+
+	
+private:
+	bson_t *m_ptr_bson;
+};
+
+#endif // LMIC_EAL_BSON_H
+
